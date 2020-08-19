4abbdbee0a2943b7a2163b1bf4af21d2207ea917
Nikita <bonnysidworker@gmail.com>
2020-08-19 22:18:37 +0400

    add HeadeBurger

--- js/bundle.js
+++ js/bundle.js
@@ -212,12 +212,23 @@ window.addEventListener('DOMContentLoaded', () => {
     const contactInfo = document.querySelector('.contact__info');
     const contact = document.querySelector('.contact .content__part.pt-65.sb');
     const footer = document.querySelector('footer .container');
+    const header = document.querySelector('.header .container');
+    const headerMenu = document.querySelector('.header__nav');
+    const headerBurger = header.querySelector('.header__burger');
+    const shadow = document.querySelector('.shadow');
 
 
+    headerBurger.addEventListener('click', (e) => {
+        e.preventDefault();
+        headerMenu.classList.toggle('active');
+        shadow.classList.toggle('active');
+        document.body.classList.toggle('lock');
+    });
 
     Object(_modules_addClassOnWindowChange__WEBPACK_IMPORTED_MODULE_0__["default"])(1160, "center", ".about .content__block", ".home .content__block");
     Object(_modules_addClassOnWindowChange__WEBPACK_IMPORTED_MODULE_0__["default"])(1160, "mt-50", ".about .content__row");
     Object(_modules_addFunctionOnWindowChange__WEBPACK_IMPORTED_MODULE_1__["default"])(1050, () => footer.before(contactInfo), () => contact.append(contactInfo));
+    Object(_modules_addFunctionOnWindowChange__WEBPACK_IMPORTED_MODULE_1__["default"])(512, () => header.after(headerMenu), () => header.append(headerMenu));
 
 });
 
